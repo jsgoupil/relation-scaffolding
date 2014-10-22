79b86980cd52b46388c49d894e5eb92f666401e3
jsgoupil <jsgoupil@gmail.com>
2014-10-22 16:01:42 -0700

    Add ModelBinder and fix the relation one 2 many to work with the binder.

---
+++ src/RelationScaffolding/RelationModelBinder.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace RelationScaffolding
+{
+    class RelationModelBinder : IModelBinder
+    {
+        public virtual object BindModel(ControllerContext controllerContext, ModelBindingContext bindingContext)
+        {
+            ValueProviderResult valueProviderResult = bindingContext.ValueProvider.GetValue(bindingContext.ModelName);
+            object model = CreateModel(controllerContext, bindingContext, bindingContext.ModelType);
+            var relationMemberLookup = new RelationMemberLookup(model);
+            ((System.Reflection.PropertyInfo)relationMemberLookup.KeyMemberInfo).SetValue(model, valueProviderResult.AttemptedValue);
+            return model;
+        }
+
+        protected virtual object CreateModel(ControllerContext controllerContext, ModelBindingContext bindingContext, Type modelType)
+        {
+            // fallback to the type's default constructor
+            Type typeToCreate = modelType;
+
+            try
+            {
+                return Activator.CreateInstance(typeToCreate);
+            }
+            catch (MissingMethodException)
+            {
+                throw;
+            }
+        }
+    }
+}
---
+++ src/RelationScaffolding/RelationModelBinderAttribute.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace RelationScaffolding
+{
+    class RelationModelBinderAttribute : CustomModelBinderAttribute
+    {
+        public override IModelBinder GetBinder()
+        {
+            return new RelationModelBinder();
+        }
+    }
+}
