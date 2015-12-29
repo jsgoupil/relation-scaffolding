714fdb0d2cd7d996417a33a8068ab2b03f9cd914
jsgoupil <jsgoupil@gmail.com>
2015-12-28 19:15:02 -0800

    Update to VS2015.

--- build.bat
+++ build.bat
@@ -1,6 +1,6 @@
 tools\nuget\nuget.exe restore RelationScaffolding.sln
 
-set MSBUILD="C:\Program Files (x86)\MSBuild\12.0\Bin\MSBuild.exe"
+set MSBUILD="C:\Program Files (x86)\MSBuild\14.0\Bin\MSBuild.exe"
 
 rd /S /Q .\src\RelationScaffolding\bin\Debug
 rd /S /Q .\src\RelationScaffolding\bin\Release
